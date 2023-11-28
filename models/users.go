4ab251f1be271c54fbdeffad62dd2cf65f628a6c
luguanzong <luguanzong@zego.im>
2023-11-28 21:19:30 +0800

    数据库字段修正

--- models/users.go
+++ models/users.go
@@ -4,6 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Users struct {
 	gorm.Model
+	Name     string `json:"name" gorm:"column:name;unique;index"`
 	Phone    string `json:"phone" gorm:"column:phone;size:20;unique"`
 	Email    string `json:"email" gorm:"column:email;size:256;unique"`
 	Password string `json:"password" gorm:"column:password"`
